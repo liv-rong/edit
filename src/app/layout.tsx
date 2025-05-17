699a1db0813068ac7c004ae1eef75f866f26df38
Upwards <1512501934@qq.com>
2025-05-18 01:15:00 +0800

    feat: init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="zh-CN"
+      className="translated-ltr"
+    >
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   )
