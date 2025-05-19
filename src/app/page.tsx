84195e3e0dc21e4cc9c8e8f06af449701c244fd6
Upwards <1512501934@qq.com>
2025-05-19 23:48:50 +0800

    feat: add node custom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,136 @@
 'use client'
+import React, { useCallback, useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+import { Editor, Transforms, Element, Node, BaseEditor } from 'slate'
+import { RenderElementProps } from 'slate-react'
+import { ReactEditor } from 'slate-react'
 
-import { Plate, usePlateEditor } from '@udecode/plate/react'
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 
-import { Editor, EditorContainer } from '@/components/ui/editor'
+type CustomElement = {
+  type: 'paragraph' | 'code'
+  children: CustomText[]
+}
 
-export default function MyEditorPage() {
-  const editor = usePlateEditor() // Initializes the editor instance
+type CustomText = {
+  text: string
+}
 
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }]
+  }
+] as CustomElement[]
+
+const CodeElement = (props: {
+  attributes: React.JSX.IntrinsicAttributes &
+    React.ClassAttributes<HTMLPreElement> &
+    React.HTMLAttributes<HTMLPreElement>
+  children:
+    | string
+    | number
+    | bigint
+    | boolean
+    | React.ReactElement<unknown, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | Promise<
+        | string
+        | number
+        | bigint
+        | boolean
+        | React.ReactPortal
+        | React.ReactElement<unknown, string | React.JSXElementConstructor<any>>
+        | Iterable<React.ReactNode>
+        | null
+        | undefined
+      >
+    | null
+    | undefined
+}) => {
   return (
-    <Plate editor={editor}>
-      {/* Provides editor context */}
-      <EditorContainer>
-        {/* Styles the editor area */}
-        <Editor placeholder="Type your amazing content here..." />
-      </EditorContainer>
-    </Plate>
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
   )
 }
+
+const DefaultElement = (props: {
+  attributes: React.JSX.IntrinsicAttributes &
+    React.ClassAttributes<HTMLParagraphElement> &
+    React.HTMLAttributes<HTMLParagraphElement>
+  children:
+    | string
+    | number
+    | bigint
+    | boolean
+    | React.ReactElement<unknown, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | Promise<
+        | string
+        | number
+        | bigint
+        | boolean
+        | React.ReactPortal
+        | React.ReactElement<unknown, string | React.JSXElementConstructor<any>>
+        | Iterable<React.ReactNode>
+        | null
+        | undefined
+      >
+    | null
+    | undefined
+}) => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()))
+  // Render the Slate context.
+
+  const renderElement = useCallback((props: RenderElementProps) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+  return (
+    <div>
+      <Slate
+        editor={editor}
+        initialValue={initialValue}
+      >
+        <Editable
+          renderElement={renderElement}
+          onKeyDown={(event) => {
+            console.log(event.key)
+            if (event.key === 'a' && event.ctrlKey) {
+              // Prevent the "`" from being inserted by default.
+              event.preventDefault()
+              console.log(' console.log(event.key)')
+              // Otherwise, set the currently selected blocks type to "code".
+              Transforms.setNodes(editor, { type: 'code' } as Partial<Node>, {
+                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n)
+              })
+            }
+          }}
+        />
+      </Slate>
+    </div>
+  )
+}
+
+export default App
